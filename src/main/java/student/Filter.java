54705392ea533e6afd04031dc92662cae6b61d8f
Jane Ren <jennyclasp@sina.com>
2025-03-09 20:21:19 -0700

    return 'contains' in alphabetically ascending order

--- src/main/java/student/Filter.java
+++ src/main/java/student/Filter.java
@@ -1,5 +1,6 @@
 package student;
 
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class Filter {
@@ -67,7 +68,8 @@ public class Filter {
         return switch (operator) {
             case EQUALS -> games.filter(game -> getter.apply(game).equalsIgnoreCase(value));
             case NOT_EQUALS -> games.filter(game -> !getter.apply(game).equalsIgnoreCase(value));
-            case CONTAINS -> games.filter(game -> getter.apply(game).toLowerCase().contains(value.toLowerCase()));
+            case CONTAINS -> games.filter(game -> getter.apply(game).toLowerCase().contains(value.toLowerCase()))
+                    .sorted(Comparator.comparing(game -> getter.apply(game).toLowerCase()));
             default -> games;
         };
     }
